90dda6456568af3fe640e24cb65a3b26831e0ed8
DevShuku <163864803+DevShuku@users.noreply.github.com>
2024-05-07 01:51:45 +0400

    Meetings Endpoint static values formatting
    Meetings Endpoint static values formatting
    
    Adding Meetings Controllers and Models
    
    Adding Meetings Controllers and Models

---
+++ Controllers/MeetingsController.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace STXAssignment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public partial class MeetingsController : ControllerBase
+    {
+        private readonly ILogger<MeetingsController> _logger;
+        public MeetingsController(ILogger<MeetingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Initialize the static List of Meetings objects
+        private static List<Meeting> _meetings = new List<Meeting>
+        {
+           new Meeting
+    {
+        MeetingId = 1,
+        Date = new DateOnly(2024, 05, 01),
+        Time = new TimeOnly(10, 30),
+        Participants = new List<Participant>
+        {
+            new Participant { Name = "Paul Adam", Role = "Sales Manager" }
+        },
+        Agenda = "Discuss new product line collaboration"
+    },
+    new Meeting
+    {
+        MeetingId = 2,
+        Date = new DateOnly(2024, 05, 02),
+        Time = new TimeOnly(11, 30),
+        Participants = new List<Participant>
+        {
+            new Participant { Name = "Sophia Lee", Role = "Supply Chain Manager" },
+            new Participant { Name = "Daniel Garcia", Role = "Finance Director" },
+            new Participant { Name = "David Lee", Role = "Logistics Coordinator" }
+        },
+        Agenda = "Review quarterly performance and plan for upcoming logistics improvements"
+    },
+    new Meeting
+    {
+        MeetingId = 3,
+        Date = new DateOnly(2024, 05, 03),
+        Time = new TimeOnly(12, 30),
+        Participants = new List<Participant>
+        {
+            new Participant { Name = "Alexandra Kim", Role = "CEO" },
+            new Participant { Name = "Matthew Brown", Role = "CFO" },
+            new Participant { Name = "Olivia Wang", Role = "Marketing Manager" }
+        },
+        Agenda = "Strategy meeting for expanding into new markets"
+      }
+    };
+
+        // Implement HTTP GET endpoint to retrieve the list of Meetings
+        [HttpGet]
+        [Route("MeetingsList")]
+        public IActionResult GetCustomers()
+        {
+            try
+            {
+                return Ok(_meetings);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+                return (IActionResult)ex;
+            }
+        }
+    }
+}
---
+++ Models/Meetings.cs
@@ -0,0 +1,29 @@
+﻿namespace STXAssignment.Controllers
+{
+    public partial class MeetingsController
+    {
+        // Define the Employee class
+        public class Meeting
+            {
+                public int MeetingId { get; set; }
+                public DateOnly Date { get; set; }
+                public TimeOnly Time { get; set; }
+                public List<Participant> Participants { get; set; }
+                public string Agenda {  get; set; }
+
+            public static implicit operator List<object>(Meeting v)
+            {
+                throw new NotImplementedException();
+            }
+
+            // Add any other properties as needed
+        }
+        }
+
+    public class Participant
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
+    
